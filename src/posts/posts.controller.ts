df842f06f57914cab98a589c2c0922919a87b9a3
udune <udune@naver.com>
2025-09-03 21:18:17 +0900

    init

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,60 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PostModel, PostsService } from './posts.service';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  // 1) GET /posts
+  // 모든 post를 다 가져온다.
+  @Get()
+  getPosts(): PostModel[] {
+    return this.postsService.getAllPosts();
+  }
+
+  // 2) GET /posts
+  // id에 해당되는 post를 가져온다.
+  // 예를 들어서 id=1일 경우 id가 1인 post를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    return this.postsService.getPostById(Number(id));
+  }
+
+  // 3) POST /posts
+  // 새로운 post를 생성한다.
+  @Post()
+  postPost(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    return this.postsService.createPost(author, title, content);
+  }
+
+  // 4) PUT /posts/:id
+  // id에 해당되는 post를 수정한다.
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    return this.postsService.updatePost(Number(id), author, title, content);
+  }
+
+  // 5) DELETE /posts/:id
+  // id에 해당되는 post를 삭제한다.
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    return this.postsService.deletePost(Number(id));
+  }
+}
---
+++ src/posts/posts.service.ts
@@ -0,0 +1,111 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '안녕하세요, 뉴진스 민지입니다!',
+    likeCount: 100,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '안녕하세요, 뉴진스 해린입니다!',
+    likeCount: 100,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 리사',
+    content: '안녕하세요, 블랙핑크 리사입니다!',
+    likeCount: 100,
+    commentCount: 999999,
+  },
+];
+
+@Injectable()
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+
+  getPostById(id: number) {
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+    return post;
+  }
+
+  updatePost(id: number, author?: string, title?: string, content?: string) {
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === id ? post : prevPost));
+
+    return post;
+  }
+
+  deletePost(id: number) {
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    posts = posts.filter((post) => post.id !== id);
+    return id;
+  }
+}
